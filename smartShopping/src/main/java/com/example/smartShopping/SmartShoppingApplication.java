0b8e36464b3223619f0309ba9e878aa6e1bf7782
longtran22 <baolong081104@gmail.com>
2025-09-27 22:24:20 +0700

    connect to digital ocean

--- smartShopping/src/main/java/com/example/smartShopping/SmartShoppingApplication.java
+++ smartShopping/src/main/java/com/example/smartShopping/SmartShoppingApplication.java
@@ -8,13 +8,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SmartShoppingApplication {
 
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(SmartShoppingApplication.class, args);
-	// }
 	public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SmartShoppingApplication.class, args);
-        String user = context.getEnvironment().getProperty("DB_USERNAME");
-        System.out.println(">>> DB_USERNAME from .env = " + user);
-    }
+		SpringApplication.run(SmartShoppingApplication.class, args);
+	}
+
 
 }
