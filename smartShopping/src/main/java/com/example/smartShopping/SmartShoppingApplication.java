831c9c0a78c18051ebda0ecaee23c70a159daff8
longtran22 <baolong081104@gmail.com>
2025-09-27 22:20:27 +0700

    Remove DB secret from properties

--- smartShopping/src/main/java/com/example/smartShopping/SmartShoppingApplication.java
+++ smartShopping/src/main/java/com/example/smartShopping/SmartShoppingApplication.java
@@ -1,13 +1,20 @@
 package com.example.smartShopping;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SmartShoppingApplication {
 
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(SmartShoppingApplication.class, args);
+	// }
 	public static void main(String[] args) {
-		SpringApplication.run(SmartShoppingApplication.class, args);
-	}
+        ConfigurableApplicationContext context = SpringApplication.run(SmartShoppingApplication.class, args);
+        String user = context.getEnvironment().getProperty("DB_USERNAME");
+        System.out.println(">>> DB_USERNAME from .env = " + user);
+    }
 
 }
